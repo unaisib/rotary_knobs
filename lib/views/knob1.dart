2ce2e6d0eeecb628654f8446cc036607bc31f09d
unaisib <unais.ib@excelledia.org>
2022-03-01 11:10:07 +0530

    started knob1

---
+++ lib/views/knob1.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class KnobOne extends StatefulWidget {
+  const KnobOne({Key? key}) : super(key: key);
+
+  @override
+  _KnobOneState createState() => _KnobOneState();
+}
+
+class _KnobOneState extends State<KnobOne> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
